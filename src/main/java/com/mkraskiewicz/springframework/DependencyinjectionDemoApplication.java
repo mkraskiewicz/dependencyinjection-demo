b4a8ff30f9e93aff0dca5a4b25d43c10a1f7b88f
Maciej Kraskiewicz <maciek.kraskiewicz@gmail.com>
2018-03-30 13:12:02 +0200

    Initial commit

---
+++ src/main/java/com/mkraskiewicz/springframework/DependencyinjectionDemoApplication.java
@@ -0,0 +1,12 @@
+package com.mkraskiewicz.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyinjectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyinjectionDemoApplication.class, args);
+	}
+}
